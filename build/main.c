e3f5dd4903c83e9065c29e8de8eda3a9c09941fa
Rychu <juzek9898@o2.pl>
2022-06-17 15:59:00 +0200

    changes in main

--- build/main.c
+++ build/main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include "../headers/buffer.h"
+#include <string.h>
 
 int main(){
-    long core_number = sysconf(_SC_NPROCESSORS_ONLN);
-    printf("%ld\n", core_number);
-    Buffer* bf = Buffer_create(core_number);
-    Pack* pc = Pack_create("SIEMA");
+    size_t buffer_size = (sysconf(_SC_NPROCESSORS_ONLN) + 1)*6;
+    Buffer* bf = Buffer_create(buffer_size);
+    char s[250] = {0};
+    for(int i =0; i < 249; ++i) 
+        s[i] = 's';
+    Pack* pc = Pack_create(s);
     Buffer_put(bf,pc);
     Pack_destroy(pc);
     char* get = Buffer_get(bf);
     printf("%s\n",get);
+    printf("%d\n",get[249]);
+    printf("%zu\n", strlen(get));
+    printf("%s\n",s);
+    printf("%d\n",s[249]);
+    printf("%zu\n", strlen(s));
     Buffer_destroy(bf);
     free(get);
     return 0;
