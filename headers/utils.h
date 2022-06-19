362b9ab4466f89d0e996e930cbb7cba8ad33bc4e
Rychu <juzek9898@o2.pl>
2022-06-19 02:10:01 +0200

    Fixing stupid utils structure

--- headers/utils.h
+++ headers/utils.h
@@ -7,21 +7,23 @@
     typedef struct Analyzer_Utils Analyzer_Utils;
     typedef struct Printer_Utils Printer_Utils;
 
-    Reader_Utils* Reader_Utils_create(Buffer** bf,Reader** rd);
-    void Reader_Utils_destroy(Reader_Utils* utils);
+    Reader_Utils* Reader_Utils_create(Buffer* bf,Buffer* logger, Reader* rd);
     Buffer* Reader_Utils_get_buffer(Reader_Utils* utils);
+    Buffer* Reader_Utils_get_logger(Reader_Utils* utils);
     Reader* Reader_Utils_get_reader(Reader_Utils* utils);
-    typedef struct Analyzer_Utils Analyzer_Utils;
+    void Reader_Utils_destroy(Reader_Utils* utils);
 
-
-    Analyzer_Utils* Analyzer_Utils_create(Buffer** bf, Analyzer** al, Results_buffer** rbf);
+    Analyzer_Utils* Analyzer_Utils_create(Buffer* bf, Buffer* logger, Analyzer* al, Results_buffer* rbf);
     Buffer* Analyzer_Utils_get_buffer(Analyzer_Utils* utils);
+    Buffer* Analyzer_Utils_get_logger(Analyzer_Utils* utils);
     Analyzer* Analyzer_Utils_get_analyzer(Analyzer_Utils* utils);
     Results_buffer* Analyzer_Utils_get_Results_buffer(Analyzer_Utils* utils);
     void Analyzer_Utils_destroy(Analyzer_Utils* utils);
 
-    Printer_Utils*  Printer_Utils_create(Results_buffer** rbf, int core_quantity);
+
+    Printer_Utils*  Printer_Utils_create(Results_buffer* rbf,Buffer* logger, int core_quantity);
     Results_buffer* Printer_Utils_get_Results_buffer(Printer_Utils* utils);
+    Buffer* Printer_Utils_get_logger(Printer_Utils* utils);
     int Printer_Utils_get_core_quantity(Printer_Utils* utils);
     void Printer_Utils_destroy(Printer_Utils* utils);
     
--- src/utils.c
+++ src/utils.c
@@ -13,55 +13,65 @@
 //Wrapery które są przekazywane do poszczególnych wątków
 
 typedef struct Reader_Utils{
-    Buffer** bf;
-    Reader** rd;
+    Buffer* bf;
+    Buffer* logger;
+    Reader* rd;
 } Reader_Utils;
 
 typedef struct Analyzer_Utils{
-    Buffer** bf;
-    Analyzer** al;
-    Results_buffer** rbf;
+    Buffer* bf;
+    Buffer* logger;
+    Analyzer* al;
+    Results_buffer* rbf;
 } Analyzer_Utils;
 
 typedef struct Printer_Utils{
-    Results_buffer** rbf;
+    Results_buffer* rbf;
+    Buffer* logger;
     int core_quantity;
     char pad[4];
 } Printer_Utils;
 
 
-Reader_Utils* Reader_Utils_create(Buffer** bf,Reader** rd);
+Reader_Utils* Reader_Utils_create(Buffer* bf,Buffer* logger, Reader* rd);
 Buffer* Reader_Utils_get_buffer(Reader_Utils* utils);
+Buffer* Reader_Utils_get_logger(Reader_Utils* utils);
 Reader* Reader_Utils_get_reader(Reader_Utils* utils);
 void Reader_Utils_destroy(Reader_Utils* utils);
 
-Analyzer_Utils* Analyzer_Utils_create(Buffer** bf, Analyzer** al, Results_buffer** rbf);
+Analyzer_Utils* Analyzer_Utils_create(Buffer* bf, Buffer* logger, Analyzer* al, Results_buffer* rbf);
 Buffer* Analyzer_Utils_get_buffer(Analyzer_Utils* utils);
+Buffer* Analyzer_Utils_get_logger(Analyzer_Utils* utils);
 Analyzer* Analyzer_Utils_get_analyzer(Analyzer_Utils* utils);
 Results_buffer* Analyzer_Utils_get_Results_buffer(Analyzer_Utils* utils);
 void Analyzer_Utils_destroy(Analyzer_Utils* utils);
 
 
-Printer_Utils*  Printer_Utils_create(Results_buffer** rbf, int core_quantity);
+Printer_Utils*  Printer_Utils_create(Results_buffer* rbf,Buffer* logger, int core_quantity);
 Results_buffer* Printer_Utils_get_Results_buffer(Printer_Utils* utils);
+Buffer* Printer_Utils_get_logger(Printer_Utils* utils);
 int Printer_Utils_get_core_quantity(Printer_Utils* utils);
-void Pinter_Utils_destroy(Printer_Utils* utils);
+void Printer_Utils_destroy(Printer_Utils* utils);
 
 
+Reader_Utils* Reader_Utils_create(Buffer* bf,Buffer* logger, Reader* rd){
+    Reader_Utils* ut = malloc(sizeof(*ut));
+    *ut = (Reader_Utils){   .bf = bf,
+                            .logger = logger,
+                            .rd = rd};
+    return ut;
+}
 
 Buffer* Reader_Utils_get_buffer(Reader_Utils* utils){
-    return *(utils->bf);
+    return utils->bf;
+}
+
+Buffer* Reader_Utils_get_logger(Reader_Utils* utils) {
+    return utils->logger;
 }
 
 Reader* Reader_Utils_get_reader(Reader_Utils* utils){
-        return *(utils->rd);
-}
-
-Reader_Utils* Reader_Utils_create(Buffer** bf,Reader** rd){
-    Reader_Utils* ut = malloc(sizeof(*ut));
-    *ut = (Reader_Utils){  .bf = bf,
-                    .rd = rd};
-    return ut;
+        return utils->rd;
 }
 
 void Reader_Utils_destroy(Reader_Utils* utils){
@@ -69,9 +79,10 @@ void Reader_Utils_destroy(Reader_Utils* utils){
 }
 
 
-Analyzer_Utils* Analyzer_Utils_create(Buffer** bf, Analyzer** al, Results_buffer** rbf){
+Analyzer_Utils* Analyzer_Utils_create(Buffer* bf, Buffer* logger, Analyzer* al, Results_buffer* rbf){
     Analyzer_Utils* ut = malloc(sizeof(*ut));
     *ut = (Analyzer_Utils){ .bf = bf,
+                            .logger = logger,
                             .al = al,
                             .rbf = rbf
     };
@@ -79,15 +90,19 @@ Analyzer_Utils* Analyzer_Utils_create(Buffer** bf, Analyzer** al, Results_buffer
 }
 
 Buffer* Analyzer_Utils_get_buffer(Analyzer_Utils* utils){
-    return *(utils->bf);
+    return utils->bf;
+}
+
+Buffer* Analyzer_Utils_get_logger(Analyzer_Utils* utils){
+    return utils->logger;
 }
 
 Analyzer* Analyzer_Utils_get_analyzer(Analyzer_Utils* utils){
-    return *(utils->al);
+    return utils->al;
 }
 
 Results_buffer* Analyzer_Utils_get_Results_buffer(Analyzer_Utils* utils){
-    return *(utils->rbf);
+    return utils->rbf;
 }
 
 void Analyzer_Utils_destroy(Analyzer_Utils* utils){
@@ -95,15 +110,20 @@ void Analyzer_Utils_destroy(Analyzer_Utils* utils){
 }
 
 
-Printer_Utils*  Printer_Utils_create(Results_buffer** rbf, int core_quantity) {
+Printer_Utils*  Printer_Utils_create(Results_buffer* rbf,Buffer* logger, int core_quantity) {
     Printer_Utils* utils = malloc(sizeof(*utils));
     *utils = (Printer_Utils){   .rbf = rbf,
+                                .logger = logger,
                                 .core_quantity = core_quantity};
     return utils;
 }
 
 Results_buffer* Printer_Utils_get_Results_buffer(Printer_Utils* utils){
-    return *(utils->rbf);
+    return utils->rbf;
+}
+
+Buffer* Printer_Utils_get_logger(Printer_Utils* utils){
+    return utils->logger;
 }
 
 void Printer_Utils_destroy(Printer_Utils* utils){
